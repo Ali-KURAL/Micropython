cc5d0d2b13fed3dbec3825c31d511c6f70c0071a
Ali KURAL <58939421+Ali-KURAL@users.noreply.github.com>
2022-03-08 12:17:08 +0300

    Update magalpha_test.py

--- libs/mag_alpha_magnetic_angle_sensor/magalpha_test.py
+++ libs/mag_alpha_magnetic_angle_sensor/magalpha_test.py
@@ -1,13 +1,16 @@
-import magalpha,time
+from mag_alpha_angle import MagAlpha
+import time
 
+magAlpha = MagAlpha(0,cs_pin = 17,sck_pin = 18,mosi_pin = 19,miso_pin = 16)
 
+#Example of register settings (some settings may not be available on every sensor model)
+#Set zero setting to 0 (Reg 0 and 1)
+magAlpha.writeRegister(0, 0)
+magAlpha.writeRegister(1, 0)
+#Set Rotation Direction to Clockwise by writting 0 to register 9
+magAlpha.writeRegister(9, 0)
 
-ma = magalpha.MagAlpha(0,17)
-
+#Read raw angle value until the user press on Ctr+C (Keyboard interrupt) to exit the program
 while True:
-    print(ma.angle)
+    angle = magAlpha.readAngleInDegree(True)
     time.sleep(0.1)
- 
-
-
-    
