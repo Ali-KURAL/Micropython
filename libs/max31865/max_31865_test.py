223495fac207b99fbb0a29b399b76d9fede8ebb4
Ali KURAL <58939421+Ali-KURAL@users.noreply.github.com>
2022-04-30 17:09:17 +0300

    Update max_31865_test.py

--- libs/max31865/max_31865_test.py
+++ libs/max31865/max_31865_test.py
@@ -1,7 +1,7 @@
 import time
 from max_31865_1shot import MAX31865
 
-mx = MAX31865(1, mosi=11, miso=12, sck=10, cs = 13)
+mx = MAX31865(1, mosi=11, miso=12, sck=10, cs=13)
 
 time.sleep(0.25)
 while True:
